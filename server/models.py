5d125e125f390a7dba4f7f1d2aee7105dac8ccb7
Gift-py <giftabumere247@gmail.com>
2024-04-12 06:30:36 +0100

    okay okay

---
+++ server/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Integer, String, Column, ForeignKey
+from sqlalchemy.orm import relationship
+
+from .db import Base
+
+class User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String, unique=True, index=True)
+    password = Column(String)
+
+    messages = relationship('Message', back_populates='sender')
+
+class Message(Base):
+    __tablename__ = 'messages'
+
+    id = Column(Integer, primary_key=True)
+    text = Column(String)
+    sender_id = Column(Integer, ForeignKey('users.id'))
+
+    sender = relationship('User', back_populates='messages')
